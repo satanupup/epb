a80ec912c4919ac28869224508a162855cdc0dcf
evalhero <evalhero@gmail.com>
2013-01-27 09:08:38 +0800

    動態鏈結

--- ConsoleApplication/ConsoleApplication34/Source.cpp
+++ ConsoleApplication/ConsoleApplication34/Source.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 using namespace std;
-
-class A
-{
+//c
+/*
+struct book{
+	int num;
+	float price;
+	struct book*next;
+};*/
+//c++
+class book{
 public:
-	A(int l){length=l;size=new char[length];}
-	~A(){delete []size;}
-	int GetLength(){return length;}
-	char&operator[](int i)
-	{
-		if(i>=0&&i<length)
-		{
-			return size[i];
-		}
-		else
-		{
-			cout<<"\n¶W¥X½d³ò¡C ";
-			return size[length-1];
-		}
-	}
-private:
-	int length;
-	char *size;
+	int num;
+	float price;
+	book*next;
 };
 
 int main()
 {
-	A a(6);
-	char *ch="hello12";
-	for(int i=0;i<8;i++)
-	{
-		a[i]=ch[i];			
-	}
-	cout<<"\n\n";
-	for(int i=0;i<8;i++)
-	{
-		cout<<a[i];
-	}
+//c
+	/*
+	void*p=malloc(sizeof(book));
+	free(p);*/
+//c++
+	book*p=new book;
+	delete p;
 
 	system("pause");
 	return 0;
